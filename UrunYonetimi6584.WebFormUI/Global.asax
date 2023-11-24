095cdf61870c63c385ebc4106280088ddebdaa9e
safahaslak <haslaksafa@gmail.com>
2023-11-24 16:43:56 +0300

    webforma başlandı

---
+++ UrunYonetimi6584.WebFormUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UrunYonetimi6584.WebFormUI.Global" Language="C#" %>
---
+++ UrunYonetimi6584.WebFormUI/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace UrunYonetimi6584.WebFormUI
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+        }
+    }
+}
